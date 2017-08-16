14765dd76ed9f2c5e6808546105b65fce7db8b6f
Shiraiwa <shiraiwa@psfc.mit.edu>
2017-08-16 17:28:22 -0400

    adjusting GUI response...

--- petram/geom/geo_plot.py
+++ petram/geom/geo_plot.py
@@ -3,6 +3,8 @@ import numpy as np
 
 def plot_geometry(viewer,  ret):
     viewer.cls()
+    viewer.get_axes()._artists[0].gl_hl_setcolor([1,0,0,])
+    
     X, cells, pt_data, cell_data, field_data = ret
     print cells.keys()
     if 'vertex' in cells:
@@ -12,7 +14,7 @@ def plot_geometry(viewer,  ret):
                     vert[:,2], 'ob',
                     array_idx = cell_data['vertex']['geometrical'],
                     linewidth = 0)
-        obj.rename('Points')
+        obj.rename('points')
         obj._artists[0].set_gl_hl_use_array_idx(True)
 
     if 'line' in cells:
@@ -20,7 +22,7 @@ def plot_geometry(viewer,  ret):
                            array_idx = cell_data['line']['geometrical'],
                            linewidth = 1.5)
 
-        obj.rename('Edges')
+        obj.rename('edges')
         obj._artists[0].set_gl_hl_use_array_idx(True)
 
     if 'triangle' in cells:
@@ -28,7 +30,7 @@ def plot_geometry(viewer,  ret):
                            array_idx = cell_data['triangle']['geometrical'],
                            linewidth = 0)
 
-        obj.rename('Faces')
+        obj.rename('faces')
         obj._artists[0].set_gl_hl_use_array_idx(True)
     
 
--- petram/geom/geom_sel_buttons.py
+++ petram/geom/geom_sel_buttons.py
@@ -6,20 +6,22 @@ fedge = get_pkg_datafile(petram.pi, 'icon', 'line.png')
 fface = get_pkg_datafile(petram.pi, 'icon', 'face.png')
 fdom = get_pkg_datafile(petram.pi, 'icon', 'domain.png')
 
+from petram.pi.sel_buttons import _select_x
+
 def select_dot(evt):
-    print(evt.GetEventObject())
+    _select_x(evt, 'point', 'point')
     
 def select_edge(evt):
-    print(evt.GetEventObject())
+    _select_x(evt, 'edge', 'edge')
     
 def select_face(evt):
-    print(evt.GetEventObject())
+    _select_x(evt, 'face', 'face')
     
-def select_dom(evt):
-    print(evt.GetEventObject())    
+def select_volume(evt):
+    _select_x(evt, 'volume', 'face')    
     
 btask = [('dot',    fdot,  2, 'select dot', select_dot),
          ('edge',   fedge, 2, 'select edge', select_edge),
          ('face',   fface, 2, 'select face', select_face),
-         ('domain', fdom,  2, 'select domain', select_dom),]
+         ('domain', fdom,  2, 'select domain', select_volume),]
             
