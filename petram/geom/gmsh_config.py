2dd368605e3ba8fee866e958b9d447d8e912737b
shiraiwa <shiraiwa@psfc.mit.edu>
2019-02-15 07:49:35 -0800

    chaged no gmsh case to warning from assertion error

--- petram/geom/gmsh_config.py
+++ petram/geom/gmsh_config.py
@@ -3,11 +3,14 @@ gmsh_init = False
 try:
    import gmsh
    has_gmsh = True   
+   if not gmsh_init:   
+       gmsh.initialize()
+   gmsh_init = True
+
 except ImportError:
    has_gmsh = False
-   assert False, "gmsh api is not found"
+   import warnings
+   warnings.warn("gmsh not found", RuntimeWarning)
+   #assert False, "gmsh api is not found"
 
-if not gmsh_init:   
-   gmsh.initialize()
-gmsh_init = True
 
